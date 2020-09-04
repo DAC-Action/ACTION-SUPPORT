e309839f40ee049cd655a6e27228946ba87c4180
cloudsheng <cloud.sheng@action-composites.com.cn>
2020-09-04 15:49:55 +0800

    Form202 Created

---
+++ Form202.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form202
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form202))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(42, 55)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 19)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "上期日期"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(42, 116)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 19)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "本期日期"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker1.Location = New System.Drawing.Point(158, 48)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 29)
+        Me.DateTimePicker1.TabIndex = 2
+        '
+        'DateTimePicker2
+        '
+        Me.DateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker2.Location = New System.Drawing.Point(158, 113)
+        Me.DateTimePicker2.Name = "DateTimePicker2"
+        Me.DateTimePicker2.Size = New System.Drawing.Size(200, 29)
+        Me.DateTimePicker2.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(158, 188)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(131, 34)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Excel"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form202
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ClientSize = New System.Drawing.Size(439, 335)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DateTimePicker2)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("宋体", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(5, 5, 5, 5)
+        Me.Name = "Form202"
+        Me.Text = "产品上下期标准成本-直接材料成本变动明细表"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents DateTimePicker2 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+End Class
---
+++ Form202.vb
@@ -0,0 +1,163 @@
+﻿Public Class Form202
+    Dim oConnection As New Oracle.ManagedDataAccess.Client.OracleConnection
+    Dim oCommand As New Oracle.ManagedDataAccess.Client.OracleCommand
+    Dim oCommand2 As New Oracle.ManagedDataAccess.Client.OracleCommand
+    Dim oCommand3 As New Oracle.ManagedDataAccess.Client.OracleCommand
+    Dim oReader As Oracle.ManagedDataAccess.Client.OracleDataReader
+    Dim oReader2 As Oracle.ManagedDataAccess.Client.OracleDataReader
+    Dim xExcel As Microsoft.Office.Interop.Excel.Application
+    Dim xWorkBook As Microsoft.Office.Interop.Excel.Workbook
+    Dim Ws As Microsoft.Office.Interop.Excel.Worksheet
+    Dim oRng As Microsoft.Office.Interop.Excel.Range
+    Dim LineZ As Integer = 0
+    Dim Time1 As Date
+    Dim Time2 As Date
+    Dim CYear1 As Int16 = 0
+    Dim CYear2 As Int16 = 0
+    Dim CMonth1 As Int16 = 0
+    Dim CMonth2 As Int16 = 0
+    Dim OldExcel() As Process = Process.GetProcessesByName("Excel")
+    Private Sub Form202_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckForIllegalCrossThreadCalls = False
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackgroundWorker1.IsBusy() Then
+            MsgBox("处理中，请等待")
+            Return
+        End If
+        Time1 = DateTimePicker1.Value
+        Time2 = DateTimePicker2.Value
+        CYear1 = Time1.Year
+        CYear2 = Time2.Year
+        CMonth1 = Time1.Month
+        CMonth2 = Time2.Month
+        If CYear1 = CYear2 And CMonth1 = CMonth2 Then
+            MsgBox("Date Error")
+            Return
+        End If
+        oConnection.ConnectionString = Module1.OpenOracleConnection("actiontest")
+        If oConnection.State <> ConnectionState.Open Then
+            Try
+                oConnection.Open()
+                oCommand.Connection = oConnection
+                oCommand.CommandType = CommandType.Text
+                oCommand2.Connection = oConnection
+                oCommand2.CommandType = CommandType.Text
+                oCommand3.Connection = oConnection
+                oCommand3.CommandType = CommandType.Text
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+        
+
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        ExportToExcel()
+    End Sub
+    Private Sub ExportToExcel()
+        xExcel = New Microsoft.Office.Interop.Excel.Application
+        xWorkBook = xExcel.Workbooks.Add()
+        Ws = xWorkBook.Sheets.Add()
+        Ws = xWorkBook.Sheets(1)
+        Ws.Activate()
+        Ws.Name = "直接材料成本差异"
+        AdjustExcelFormat()
+        LineZ = 2
+        oCommand.CommandText = "Select * from ( "
+        oCommand.CommandText += "Select bma01,xx1,bma06,bmb03,xx2,ima08,bmb10,ima25,t1,sum(n1) as n1,sum(n2) as n2 ,sum(m1) as m1,sum(m2) as m2,bma05, bmaacti from ( "
+        oCommand.CommandText += "Select bma01,s1.ima02 as xx1,bma06, bmb03, s2.ima02 as xx2,s2.ima08, bmb10, s2.ima25,Round((1/bmb10_fac),2) as t1, Round(sum(bmb06/bmb07 * (1+bmb08 /100)),2) as n1, stb07 / Round((1/bmb10_fac),2) as n2, 0 as m1, 0 as m2,bma05, bmaacti "
+        oCommand.CommandText += "from bma_file left join bmb_file on bma01 = bmb01 and bma06 = bmb29 left join ima_file s1 on bma01 = s1.ima01 and bmb01 = s1.ima01 left join ima_file s2 on bmb03 = s2.ima01 "
+        oCommand.CommandText += "left join stb_file on bmb03 = stb01 and stb02 = " & CYear1 & " and stb03 = " & CMonth1 & " where s1.ima08 in ('M','S') and bmaacti = 'Y' and bma05 <= to_date('"
+        oCommand.CommandText += Time2.ToString("yyyy/MM/dd") & "','yyyy/mm/dd') and not (length(bmb03) in (15, 16) and s2.ima08 = 'M') and bmb04 <= to_date('"
+        oCommand.CommandText += Time1.ToString("yyyy/MM/dd") & "','yyyy/mm/dd') and (bmb05 is null or bmb05 >= to_date('"
+        oCommand.CommandText += Time1.ToString("yyyy/MM/dd") & "','yyyy/mm/dd')) and bmb07 <> 0 group by bma01,s1.ima02,bma06, bmb03, s2.ima02,s2.ima08, bmb10, s2.ima25,(1/bmb10_fac), stb07,bma05, bmaacti "
+        oCommand.CommandText += "union all "
+        oCommand.CommandText += "Select bma01,s1.ima02,bma06, bmb03, s2.ima02,s2.ima08, bmb10, s2.ima25,Round((1/bmb10_fac),2) as t1,0,0, Round(sum(bmb06/bmb07 * (1+bmb08 /100)),2), stb07 / Round((1/bmb10_fac),2),bma05, bmaacti "
+        oCommand.CommandText += "from bma_file left join bmb_file on bma01 = bmb01 and bma06 = bmb29 left join ima_file s1 on bma01 = s1.ima01 and bmb01 = s1.ima01 left join ima_file s2 on bmb03 = s2.ima01 "
+        oCommand.CommandText += "left join stb_file on bmb03 = stb01 and stb02 = " & CYear2 & " and stb03 = " & CMonth2 & " where s1.ima08 in ('M','S') and bmaacti = 'Y' and bma05 <= to_date('"
+        oCommand.CommandText += Time2.ToString("yyyy/MM/dd") & "','yyyy/mm/dd') and not (length(bmb03) in (15, 16) and s2.ima08 = 'M') and bmb04 <= to_date('"
+        oCommand.CommandText += Time2.ToString("yyyy/MM/dd") & "','yyyy/mm/dd') and (bmb05 is null or bmb05 >= to_date('"
+        oCommand.CommandText += Time2.ToString("yyyy/MM/dd") & "','yyyy/mm/dd')) and bmb07 <> 0 group by bma01,s1.ima02,bma06, bmb03, s2.ima02,s2.ima08, bmb10, s2.ima25,(1/bmb10_fac), stb07,bma05, bmaacti "
+        oCommand.CommandText += ") group by bma01,xx1,bma06,bmb03,xx2,ima08,bmb10,ima25,t1,bma05, bmaacti ) where n1 <> m1 or n2 <> m2 order by bma01, bmb03"
+
+        oReader = oCommand.ExecuteReader
+        If oReader.HasRows() Then
+            While oReader.Read()
+                For i As Int16 = 0 To 12 Step 1
+                    Ws.Cells(LineZ, 1 + i) = oReader.Item(i)
+                Next
+                Ws.Cells(LineZ, 14) = "=(L" & LineZ & "-J" & LineZ & ") * K" & LineZ
+                Ws.Cells(LineZ, 15) = "=(M" & LineZ & "-K" & LineZ & ") * L" & LineZ
+                Ws.Cells(LineZ, 16) = "=(L" & LineZ & "*M" & LineZ & ")-(J" & LineZ & "*K" & LineZ & ")"
+                Ws.Cells(LineZ, 17) = oReader.Item("bma05")
+                Ws.Cells(LineZ, 18) = oReader.Item("bmaacti")
+                LineZ += 1
+            End While
+        End If
+
+
+        oRng = Ws.Range("A1", "R1")
+        oRng.EntireColumn.AutoFit()
+
+
+    End Sub
+    Private Sub AdjustExcelFormat()
+        xExcel.ActiveWindow.Zoom = 75
+        Ws.Rows.EntireRow.RowHeight = 16
+        Ws.Cells(1, 1) = "主件料号"
+        Ws.Cells(1, 2) = "品名"
+        Ws.Cells(1, 3) = "特性代码"
+        Ws.Cells(1, 4) = "元件料号"
+        Ws.Cells(1, 5) = "品名"
+        oRng = Ws.Range("D1", "D1")
+        oRng.EntireColumn.NumberFormatLocal = "@"
+        Ws.Cells(1, 6) = "来源码"
+        Ws.Cells(1, 7) = "BOM表单位"
+        Ws.Cells(1, 8) = "库存单位"
+        Ws.Cells(1, 9) = "换算率"
+        oRng = Ws.Range("I1", "P1")
+        oRng.EntireColumn.NumberFormatLocal = "#,##0.00_ "
+        Ws.Cells(1, 10) = "上期用量"
+        Ws.Cells(1, 11) = "上期标准成本单价"
+        Ws.Cells(1, 12) = "本期用量"
+        Ws.Cells(1, 13) = "本期标准成本单价"
+        Ws.Cells(1, 14) = "用量差异"
+        Ws.Cells(1, 15) = "标准单价差异"
+        Ws.Cells(1, 16) = "成本差异"
+        Ws.Cells(1, 17) = "BOM表发放日期"
+        Ws.Cells(1, 18) = "BOM表有效否"
+        oRng = Ws.Range("B2", "B2")
+        oRng.Select()
+        'Ws.FreezePanes(2, 5)
+        xExcel.ActiveWindow.FreezePanes = True
+    End Sub
+    Private Sub BackgroundWorker1_RunWorkCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        SaveExcel()
+    End Sub
+    Private Sub SaveExcel()
+        SaveFileDialog1.FileName = "产品上下期标准成本-直接材料成本变动明细表"
+        SaveFileDialog1.DefaultExt = ".xlsx"
+        Dim SON As DialogResult = SaveFileDialog1.ShowDialog()
+        If SON = DialogResult.OK Then
+            Dim SFN As String = SaveFileDialog1.FileName
+            Ws.SaveAs(SFN, XlFileFormat.xlOpenXMLWorkbook)
+        Else
+            MsgBox("没有储存文件", MsgBoxStyle.Critical)
+        End If
+        xWorkBook.Saved = True
+        xWorkBook.Close()
+        xExcel.Quit()
+        If oConnection.State = ConnectionState.Open Then
+            Try
+                oConnection.Close()
+                Module1.KillExcelProcess(OldExcel)
+                MsgBox("Finished")
+            Catch ex As Exception
+                MsgBox(ex.Message())
+            End Try
+        End If
+    End Sub
+End Class
