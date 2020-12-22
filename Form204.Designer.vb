69fc91c63cd6157e5d6f9ed8f9fdc643fe7bc558
cloudsheng <cloud.sheng@action-composites.com.cn>
2020-12-22 13:39:29 +0800

    Form204 Create

---
+++ Form204.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form204
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form204))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ComboBox2)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Font = New System.Drawing.Font("微软雅黑", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(27, 21)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2)
+        Me.GroupBox1.Size = New System.Drawing.Size(582, 174)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "搜寻条件"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(120, 101)
+        Me.ComboBox2.Margin = New System.Windows.Forms.Padding(2)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(411, 29)
+        Me.ComboBox2.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(50, 103)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 21)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "品号"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(50, 38)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 21)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "客户"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.Font = New System.Drawing.Font("微软雅黑", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(120, 36)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(411, 29)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("微软雅黑", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button1.Location = New System.Drawing.Point(147, 218)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(121, 41)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Excel"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(509, 229)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(19, 19)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "0"
+        '
+        'Form204
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ClientSize = New System.Drawing.Size(633, 270)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Font = New System.Drawing.Font("宋体", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(5)
+        Me.Name = "Form204"
+        Me.Text = "Routing Data"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+End Class
---
+++ Form204.vb
@@ -0,0 +1,200 @@
+﻿Imports Microsoft.Office.Interop.Excel.XlFileFormat
+Imports Microsoft.Office.Interop.Excel.XlAutoFillType
+Imports Microsoft.Office.Interop.Excel.Constants
+Public Class Form204
+    Dim mConnection As New SqlClient.SqlConnection
+    Dim mSQLS1 As New SqlClient.SqlCommand
+    Dim mSQLS2 As New SqlClient.SqlCommand
+    Dim mSQLReader As SqlClient.SqlDataReader
+    Dim oConnection As New Oracle.ManagedDataAccess.Client.OracleConnection
+    Dim oCommand As New Oracle.ManagedDataAccess.Client.OracleCommand
+    Dim tModel_type As String
+    Dim tModel As String
+    Dim xExcel As Microsoft.Office.Interop.Excel.Application
+    Dim xWorkBook As Microsoft.Office.Interop.Excel.Workbook
+    Dim Ws As Microsoft.Office.Interop.Excel.Worksheet
+    Dim oRng As Microsoft.Office.Interop.Excel.Range
+    Dim LineZ As Integer = 0
+    Dim OldExcel() As Process = Process.GetProcessesByName("Excel")
+    Private Sub Form204_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckForIllegalCrossThreadCalls = False
+        mConnection.ConnectionString = Module1.OpenConnectionOfMes()
+        oConnection.ConnectionString = Module1.OpenOracleConnection("actiontest")
+        If mConnection.State <> ConnectionState.Open Then
+            Try
+                mConnection.Open()
+                mSQLS1.Connection = mConnection
+                mSQLS1.CommandType = CommandType.Text
+                mSQLS2.Connection = mConnection
+                mSQLS2.CommandType = CommandType.Text
+                oConnection.Open()
+                oCommand.Connection = oConnection
+                oCommand.CommandType = CommandType.Text
+            Catch ex As Exception
+                MsgBox(ex.Message())
+            End Try
+        End If
+        BindModel_Type()
+        Dim Model_Type As String = String.Empty
+        BindModel(Model_Type)
+    End Sub
+    Private Sub BindModel_Type()
+        Me.ComboBox1.Items.Clear()
+        mSQLS1.CommandText = "SELECT * FROM model_type WHERE model_type <> 'Action'"
+        mSQLReader = mSQLS1.ExecuteReader()
+        If mSQLReader.HasRows() Then
+            While mSQLReader.Read()
+                Me.ComboBox1.Items.Add(mSQLReader.Item(0).ToString())
+            End While
+        End If
+        mSQLReader.Close()
+    End Sub
+    Private Sub BindModel(ByVal Models1 As String)
+        Me.ComboBox2.Items.Clear()
+        mSQLS1.CommandText = "select distinct lot.model,model.modelname  from lot,model " _
+                          & " where lot.model = model.model and model.model_type <> 'Action'"
+        If Not String.IsNullOrEmpty(Models1) Then
+            mSQLS1.CommandText += " AND model.model_type = '" & Models1 & "'"
+        End If
+        mSQLReader = mSQLS1.ExecuteReader()
+        If mSQLReader.HasRows() Then
+            While mSQLReader.Read()
+                Me.ComboBox2.Items.Add(mSQLReader.Item(0).ToString() & "|" & mSQLReader.Item(1).ToString())
+            End While
+        End If
+        mSQLReader.Close()
+    End Sub
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Dim model_type As String = ComboBox1.Items(ComboBox1.SelectedIndex).ToString()
+        BindModel(model_type)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackgroundWorker1.IsBusy() Then
+            MsgBox("处理中，请等待")
+            Return
+        End If
+        tModel_type = String.Empty
+        tModel = String.Empty
+        If mConnection.State <> ConnectionState.Open Then
+            Try
+                mConnection.Open()
+                mSQLS1.Connection = mConnection
+                mSQLS1.CommandType = CommandType.Text
+            Catch ex As Exception
+                MsgBox(ex.Message())
+            End Try
+        End If
+        If Not IsNothing(ComboBox1.SelectedItem) Then
+            tModel_type = ComboBox1.SelectedItem.ToString()
+        End If
+        If Not IsNothing(ComboBox2.SelectedItem) Then
+            tModel = ComboBox2.SelectedItem.ToString()
+            Dim stCount As Int16 = Strings.InStr(tModel, "|")
+            If stCount > 0 Then
+                tModel = Strings.Left(tModel, stCount - 1)
+            End If
+        End If
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        ExportToExcel()
+    End Sub
+    Private Sub SaveExcel()
+        SaveFileDialog1.FileName = "Routing_Data"
+        SaveFileDialog1.DefaultExt = ".xlsx"
+        Dim SON As DialogResult = SaveFileDialog1.ShowDialog()
+        If SON = DialogResult.OK Then
+            Dim SFN As String = SaveFileDialog1.FileName
+            Ws.SaveAs(SFN, XlFileFormat.xlOpenXMLWorkbook)
+        Else
+            MsgBox("没有储存文件", MsgBoxStyle.Critical)
+        End If
+        xWorkBook.Saved = True
+        xWorkBook.Close()
+        xExcel.Quit()
+        If mConnection.State = ConnectionState.Open Then
+            Try
+                mConnection.Close()
+                Module1.KillExcelProcess(OldExcel)
+            Catch ex As Exception
+                MsgBox(ex.Message())
+            End Try
+        End If
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        SaveExcel()
+    End Sub
+    Private Sub ExportToExcel()
+        xExcel = New Microsoft.Office.Interop.Excel.Application
+        xWorkBook = xExcel.Workbooks.Add()
+        Ws = xWorkBook.Sheets(1)
+        AdjustExcelFormat()
+        mSQLS1.CommandText = "SELECT model.model,model.modelname,model_station_paravalue.cf01,routing.route,routing.seq,routing.station,station.stationname,model_paravalue.value  FROM MODEL "
+        mSQLS1.CommandText += "left join model_paravalue on model.model = model_paravalue.model and model_paravalue.parameter = 'Accessory' "
+        mSQLS1.CommandText += "LEFT JOIN ROUTING ON MODEL.default_route = ROUTING.ROUTE left join model_station_paravalue on model_station_paravalue.model = model.model and model_station_paravalue.profilename = 'ERP' "
+        mSQLS1.CommandText += "and model_station_paravalue.station = routing.station left join station on station.station = model_station_paravalue.station and station.station = routing.station "
+        mSQLS1.CommandText += "Where model.default_route not in ( Select distinct route from routing where seq = 1 and station <> '0080' ) and routing.station is not null and cf01 is not null "
+        If Not String.IsNullOrEmpty(tModel_type) Then
+            mSQLS1.CommandText += "and model.model_type like '" & tModel_type & "' "
+        End If
+        If Not String.IsNullOrEmpty(tModel) Then
+            mSQLS1.CommandText += "and model.model like '" & tModel & "' "
+        End If
+        mSQLS1.CommandText += " order by model.model,routing.seq"
+        mSQLReader = mSQLS1.ExecuteReader()
+        If mSQLReader.HasRows() Then
+            While mSQLReader.Read()
+                Ws.Cells(LineZ, 1) = mSQLReader.Item("model")
+                Ws.Cells(LineZ, 2) = mSQLReader.Item("modelname")
+                If Not IsDBNull(mSQLReader.Item("cf01")) Then
+                    Dim ERPPN As String = mSQLReader.Item("cf01")
+                    Ws.Cells(LineZ, 3) = ERPPN
+                    oCommand.CommandText = "Select nvl(imaud02,' ') from ima_file where ima01 = '" & ERPPN & "' "
+                    Dim l_imaud02 As String = oCommand.ExecuteScalar()
+                    Ws.Cells(LineZ, 4) = l_imaud02
+                    If Not String.IsNullOrWhiteSpace(l_imaud02) Then
+                        oCommand.CommandText = "Select gem02 from gem_file where gem01 = '" & l_imaud02 & "' "
+                        Dim l_gem02 As String = oCommand.ExecuteScalar()
+                        Ws.Cells(LineZ, 5) = l_gem02
+                    End If
+                End If
+                Ws.Cells(LineZ, 6) = mSQLReader.Item("route")
+                Ws.Cells(LineZ, 7) = mSQLReader.Item("seq")
+                Ws.Cells(LineZ, 8) = mSQLReader.Item("station")
+                Ws.Cells(LineZ, 9) = mSQLReader.Item("stationname")
+                mSQLS2.CommandText = "Select IETime  from ERPSUPPORT.dbo.FIIETIME where ModelID = '" & mSQLReader.Item("model") & "' and StationCode = '" & mSQLReader.Item("station") & "'"
+                Dim IET As Decimal = mSQLS2.ExecuteScalar()
+                Ws.Cells(LineZ, 10) = IET
+                Ws.Cells(LineZ, 11) = mSQLReader.Item("value")
+                LineZ += 1
+                Label3.Text = LineZ
+                Label3.Refresh()
+            End While
+        End If
+        mSQLReader.Close()
+        oConnection.Close()
+
+    End Sub
+    Private Sub AdjustExcelFormat()
+        xExcel.ActiveWindow.Zoom = 75
+        Ws.Name = "Routing资料"
+        oRng = Ws.Range("A1", "H1")
+        oRng.EntireColumn.ColumnWidth = 18.2
+        Ws.Cells(1, 1) = "model"
+        Ws.Cells(1, 2) = "modelname"
+        Ws.Cells(1, 3) = "ERP"
+        Ws.Cells(1, 4) = "ERP生产部门代码"
+        Ws.Cells(1, 5) = "ERP生产部门名称"
+        Ws.Cells(1, 6) = "Routing"
+        Ws.Cells(1, 7) = "seq"
+        Ws.Cells(1, 8) = "station"
+        Ws.Cells(1, 9) = "stationname"
+        Ws.Cells(1, 10) = "MES作业站标准人工工时"
+        Ws.Cells(1, 11) = "Accessory"
+        oRng = Ws.Range("H1", "H1")
+        oRng.EntireColumn.NumberFormatLocal = "@"
+        LineZ = 2
+    End Sub
+End Class
