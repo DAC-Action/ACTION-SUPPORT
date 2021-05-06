f428fa8a289d4a9b440e92a1431fb1c8b3523ba7
cloudsheng <cloud.sheng@action-composites.com.cn>
2021-05-06 08:15:42 +0800

    Form205 Created

---
+++ Form205.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form205
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form205))
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.FlowLayoutPanel2 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        Me.FlowLayoutPanel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.Controls.Add(Me.Label1)
+        Me.FlowLayoutPanel1.Controls.Add(Me.Label2)
+        Me.FlowLayoutPanel1.Controls.Add(Me.Label3)
+        Me.FlowLayoutPanel1.Controls.Add(Me.Label4)
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(13, 13)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(238, 113)
+        Me.FlowLayoutPanel1.TabIndex = 0
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(285, 34)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(142, 52)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "打开Excel"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 19)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "要求如下"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(172, 19)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "1. 资料页签为明细"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(3, 38)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(230, 19)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "2. 第1行和第5行不能空白"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(3, 57)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(200, 19)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "3.其余按照原格式不变"
+        '
+        'FlowLayoutPanel2
+        '
+        Me.FlowLayoutPanel2.Controls.Add(Me.Label5)
+        Me.FlowLayoutPanel2.Controls.Add(Me.Label6)
+        Me.FlowLayoutPanel2.Controls.Add(Me.Label7)
+        Me.FlowLayoutPanel2.Controls.Add(Me.Label8)
+        Me.FlowLayoutPanel2.Location = New System.Drawing.Point(13, 169)
+        Me.FlowLayoutPanel2.Name = "FlowLayoutPanel2"
+        Me.FlowLayoutPanel2.Size = New System.Drawing.Size(238, 55)
+        Me.FlowLayoutPanel2.TabIndex = 1
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(3, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(142, 19)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "开启资料笔数："
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(151, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(19, 19)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "0"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(3, 19)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(142, 19)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "上传资料笔数："
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(151, 19)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(19, 19)
+        Me.Label8.TabIndex = 3
+        Me.Label8.Text = "0"
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(285, 164)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(142, 60)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "上传资料"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form205
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ClientSize = New System.Drawing.Size(772, 483)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.FlowLayoutPanel2)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("宋体", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(5, 5, 5, 5)
+        Me.Name = "Form205"
+        Me.Text = "汇入FIIETIME"
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.FlowLayoutPanel1.PerformLayout()
+        Me.FlowLayoutPanel2.ResumeLayout(False)
+        Me.FlowLayoutPanel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents FlowLayoutPanel1 As System.Windows.Forms.FlowLayoutPanel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents FlowLayoutPanel2 As System.Windows.Forms.FlowLayoutPanel
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+End Class
---
+++ Form205.vb
@@ -0,0 +1,90 @@
+﻿Public Class Form205
+    Dim mConnection As New SqlClient.SqlConnection
+    Dim mSQLS1 As New SqlClient.SqlCommand
+    Dim DS As Data.DataSet = New DataSet()
+    Dim RowsCount As Integer = 0
+    Dim RowsCount1 As Integer = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        OpenFileDialog1.Title = "OPEN EXCEL"
+        Dim selectOK As DialogResult = OpenFileDialog1.ShowDialog()
+        If selectOK = System.Windows.Forms.DialogResult.OK Then
+            Dim ExcelPath As String = OpenFileDialog1.FileName
+            Dim Source As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source = " & ExcelPath & ";Extended Properties = 'Excel 12.0;HDR=YES';"
+            Dim Excelconn As OleDb.OleDbConnection = New OleDb.OleDbConnection(Source)
+            Excelconn.Open()
+            Dim ExcelString = "SELECT * FROM [明细$]"
+            Dim ExcelAdapater As OleDb.OleDbDataAdapter = New OleDb.OleDbDataAdapter(ExcelString, Excelconn)
+            'Dim DS As Data.DataSet = New DataSet()
+            Try
+                ExcelAdapater.Fill(DS, "table1")
+            Catch ex As Exception
+                MsgBox(ex.Message())
+            End Try
+        End If
+        RowsCount = 0
+        RowsCount = DS.Tables("table1").Rows.Count
+        Label6.Text = RowsCount
+        If RowsCount > 0 Then
+            Button2.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        RowsCount1 = 0
+        Dim g_success As Boolean = True
+        Dim Tran1 As SqlClient.SqlTransaction = mConnection.BeginTransaction()
+        mSQLS1.Transaction = Tran1
+        For i As Integer = 0 To RowsCount - 1 Step 1
+            ' 檢查是否第1行和第5行是空白
+            If IsDBNull(DS.Tables("table1").Rows(i).Item(0)) Then
+                g_success = False
+                Tran1.Rollback()
+                Exit For
+            End If
+            If IsDBNull(DS.Tables("table1").Rows(i).Item(4)) Then
+                g_success = False
+                Tran1.Rollback()
+                Exit For
+            End If
+            mSQLS1.CommandText = "DELETE FIIETIME Where ModelID = '" & DS.Tables("table1").Rows(i).Item(0).ToString() & "' AND StationCode = '" & DS.Tables("table1").Rows(i).Item(4) & "'"
+            Try
+                mSQLS1.ExecuteNonQuery()
+            Catch ex As Exception
+                'MsgBox(ex.Message())
+            End Try
+
+            mSQLS1.CommandText = "INSERT INTO FIIETIME VALUES ('" & DS.Tables("table1").Rows(i).Item(0).ToString() & "','" & DS.Tables("table1").Rows(i).Item(1).ToString() & "','" & DS.Tables("table1").Rows(i).Item(3).ToString()
+            mSQLS1.CommandText += "'," & DS.Tables("table1").Rows(i).Item(6).ToString() & ",'" & DS.Tables("table1").Rows(i).Item(4).ToString() & "','" & DS.Tables("table1").Rows(i).Item(5).ToString() & "')"
+            Try
+                mSQLS1.ExecuteNonQuery()
+                RowsCount1 += 1
+                Label8.Text = RowsCount1
+                Label8.Refresh()
+            Catch ex As Exception
+                MsgBox(ex.Message())
+                g_success = False
+                Tran1.Rollback()
+                Exit For
+            End Try
+        Next
+        If g_success = True Then
+            Tran1.Commit()
+            Button2.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Form205_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckForIllegalCrossThreadCalls = False
+        mConnection.ConnectionString = Module1.OpenConnectionOfERPSUPPORT()
+        
+        If mConnection.State <> ConnectionState.Open Then
+            Try
+                mConnection.Open()
+                mSQLS1.Connection = mConnection
+                mSQLS1.CommandType = CommandType.Text
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+    End Sub
+End Class
